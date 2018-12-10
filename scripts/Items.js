1437cdff6c3f7f8cd111c47a99b1434999fc218f
Jordan Heffernan <jheff2@gmail.com>
2018-12-10 09:22:17 -0800

    added create item and name validate functions to item.js

--- scripts/Items.js
+++ scripts/Items.js
@@ -1,8 +1,23 @@
 /* eslint-disable strict */
-const Items = (function () {
+const Item = (function () {
   const foo = 'bar';
-  
+
+const validateName = function (name) {
+  if(!name) {
+    throw(new TypeError("Name does not exist."))
+  }
+}
+
+const create = function (name) {
   return {
-    
+    id: cuid(),
+    name: name,
+    checked: false
+  }
+}
+
+  return {
+    validateName,
+    create,
   };
 }() );
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  shoppingList.render();
 });
 
-console.log(Items);
-console.log(foo);
